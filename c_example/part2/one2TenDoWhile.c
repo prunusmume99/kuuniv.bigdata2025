d220a781427f2d769f914645f20309dedca22b25
prunusmume99 <prunusmume99@gmail.com>
2025-03-07 16:01:50 +0900

    part2 update 3.7

---
+++ c_example/part2/one2TenDoWhile.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void)
+{
+    int i = 0;
+    do
+    {
+        printf("%d 번 출력입니다.\n", i + 1); 
+        ++i;
+    }   while (i < 10) ;
+    return 0;
+}
---
+++ c_example/part2/scoreGrade.c
@@ -0,0 +1,22 @@
+// A - F 까지 점수를 받아서 출력
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void)
+{
+    char grades[] = {'F','F','F','F','F','F','D','C','B','A','\0'};
+    int score;
+    char grade = 'F';
+    while(true)
+    {
+        printf("점수를 넣으세요 : ");
+        scanf( "%d", &score);
+        grade = 'F';
+
+        if (score > 0 && score <=100){
+            grade = grades [score / 10];
+        }
+        printf("점수는 : %d --- %c\n", score, grade);
+    }
+    return 0;
+}
